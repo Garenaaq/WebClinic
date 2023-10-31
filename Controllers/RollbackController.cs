1703f238172ffd7ace22f44aa4e3b7cffb6197ea
Garenaaq <GlebSpoke@gmail.com>
2023-11-01 02:36:09 +0300

    Админку полностью доделал, откаты тоже. Только откаты по количеству не сделал

--- Controllers/RollbackController.cs
+++ Controllers/RollbackController.cs
@@ -6,9 +6,25 @@ namespace WebClinic.Controllers
 {
     public class RollbackController : Controller
     {
+        ClinicContext _db;
+        public RollbackController(ClinicContext db) 
+        {
+            _db = db;
+        }
+
+        [HttpGet]
         public ActionResult Index()
         {
-            return View();
+            List<Rollback> objRollback = _db.Rollback.ToList();
+            return View(objRollback);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult RollBack(string time) 
+        {
+            _db.Database.ExecuteSqlRaw($"CALL func_back('{time}')");
+            return RedirectToAction("Index");
         }
     }
 }
